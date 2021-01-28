1348769f66a2c11fce5867f76b5bca897a4d372d
Cheng Pham <chengpham@gmail.com>
2021-01-27 23:00:29 -0800

    update

--- db/migrate/20210128051234_create_users.rb
+++ db/migrate/20210128051234_create_users.rb
@@ -1,8 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :first_name
-      t.string :last_name
+      t.string :name
       t.string :email, index: {unique: true}
       t.string :password_digest
       t.boolean :admin, default: false

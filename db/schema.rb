685a372d050d0fdbbed185261841fda4e0dd8abe
Cheng Pham <chengpham@gmail.com>
2021-01-27 23:17:00 -0800

    update

--- db/schema.rb
+++ db/schema.rb
@@ -31,8 +31,7 @@ ActiveRecord::Schema.define(version: 2021_01_28_060151) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
+    t.string "name"
     t.string "email"
     t.string "password_digest"
     t.boolean "admin", default: false
